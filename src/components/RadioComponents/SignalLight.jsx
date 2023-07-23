b10665b39b72c7e675f7303d329b87b8dbe88c9f
Abhishek <67749693+abhishekY495@users.noreply.github.com>
2023-07-23 23:16:15 +0530

    Styling

--- src/components/RadioComponents/SignalLight.jsx
+++ src/components/RadioComponents/SignalLight.jsx
@@ -1,14 +1,22 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 export default function SignalLight() {
+  const radioStationsData = useSelector((state) => state.radioStations);
+  const { selectedStation } = radioStationsData;
+
   return (
     <div
-      className="h-6 w-6 bg-red-500
-        absolute -top-[24px] left-8
-        border border-red-600
-        shadow-red-500 shadow-[0px_10px_50px_20px]
-        rounded-t-full
-        hover:cursor-pointer hover:animate-none"
+      className={`
+        ${
+          selectedStation
+            ? "bg-red-500 border-red-600 shadow-red-500 shadow-[0px_10px_50px_20px]"
+            : "bg-[#101010] border-[3px] border-red-800/50 shadow-red-800 shadow-[0px_5px_80px_5px]"
+        }
+        h-6 w-6 border border-b-0
+        absolute -top-[25px] left-8
+        rounded-t-full transition-all
+        hover:cursor-pointer`}
     ></div>
   );
 }
