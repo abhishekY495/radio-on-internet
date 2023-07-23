75c2d9214ed2c2b74cb2d27cd626a61504701535
Abhishek <67749693+abhishekY495@users.noreply.github.com>
2023-07-23 23:17:23 +0530

    Feat: Station info

--- src/components/RadioComponents/StationInfo.jsx
+++ src/components/RadioComponents/StationInfo.jsx
@@ -1,9 +1,30 @@
 import React from "react";
+import { useSelector } from "react-redux";
+
+import defaultFavicon from "../../assets/radio.png";
 
 export default function StationInfo() {
+  const radioStationsData = useSelector((state) => state.radioStations);
+  const { selectedStation } = radioStationsData;
+
   return (
-    <div className="bg-neutral-400/50 w-full h-[15%]">
-      StationInfo
+    <div className="flex gap-1 p-1 bg-neutral-400/50 h-[15%]">
+      {selectedStation?.favicon ? (
+        <img
+          src={selectedStation?.favicon}
+          className="w-[80px] object-cover bg-black/50 p-1"
+          alt={selectedStation?.name}
+        />
+      ) : (
+        selectedStation && (
+          <img
+            src={defaultFavicon}
+            className="w-[80px] object-cover bg-black/50 p-3"
+            alt={selectedStation?.name}
+          />
+        )
+      )}
+      <p className="truncate">{selectedStation?.name}</p>
     </div>
   );
 }
