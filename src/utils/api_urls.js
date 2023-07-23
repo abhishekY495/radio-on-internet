02554c2f72006664e309ab4c7ea631578bd1e7aa
Abhishek <67749693+abhishekY495@users.noreply.github.com>
2023-07-23 23:05:16 +0530

    Created utils

---
+++ src/utils/api_urls.js
@@ -0,0 +1,4 @@
+export const COUNTRIES_API_URL =
+  "http://de1.api.radio-browser.info/json/countries";
+export const RADIO_STATIONS_API_URL =
+  "http://de1.api.radio-browser.info/json/stations/bycountryexact/";
