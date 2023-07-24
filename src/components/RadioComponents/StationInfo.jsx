54025d3485b95addcb48c9d2697b9702baf14023
Abhishek <67749693+abhishekY495@users.noreply.github.com>
2023-07-24 12:34:53 +0530

    Refactor and Styling

--- src/components/RadioComponents/StationInfo.jsx
+++ src/components/RadioComponents/StationInfo.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 
 import defaultFavicon from "../../assets/radio.png";
@@ -6,25 +6,27 @@ import defaultFavicon from "../../assets/radio.png";
 export default function StationInfo() {
   const radioStationsData = useSelector((state) => state.radioStations);
   const { selectedStation } = radioStationsData;
+  const [imageError, setImageError] = useState(false);
+
+  console.log(selectedStation?.url_resolved)
+
+  useEffect(() => {
+    setImageError(false);
+  }, [selectedStation]);
 
   return (
     <div className="flex gap-1 p-1 bg-neutral-400/50 h-[15%]">
-      {selectedStation?.favicon ? (
-        <img
-          src={selectedStation?.favicon}
-          className="w-[80px] object-cover bg-black/50 p-1"
-          alt={selectedStation?.name}
-        />
-      ) : (
-        selectedStation && (
+      {selectedStation && (
+        <>
           <img
-            src={defaultFavicon}
-            className="w-[80px] object-cover bg-black/50 p-3"
-            alt={selectedStation?.name}
+            src={imageError ? defaultFavicon : selectedStation?.favicon}
+            className="w-[70px] bg-black/50 p-2 rounded transition-all"
+            alt=""
+            onError={() => setImageError(true)}
           />
-        )
+          <p className="truncate">{selectedStation?.name}</p>
+        </>
       )}
-      <p className="truncate">{selectedStation?.name}</p>
     </div>
   );
 }
