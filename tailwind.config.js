42fd03c82f147cbfec4018298518f5a29edcfc60
Abhishek <67749693+abhishekY495@users.noreply.github.com>
2023-07-24 18:09:39 +0530

    Added custom animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        pulsefast: {
+          "100%": { opacity: "0.5" },
+        },
+      },
+      animation: {
+        "pulse-fast": "pulsefast .1s linear infinite",
+      },
+    },
   },
   plugins: [],
 };
