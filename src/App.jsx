289e1adcea139e3a0d47b9cb9387fcb9310b7792
Abhishek <67749693+abhishekY495@users.noreply.github.com>
2023-07-24 12:14:51 +0530

    Added Toaster

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import { Toaster } from "react-hot-toast";
+
 import Radio from "./components/Radio";
 
 function App() {
-  return <Radio />;
+  return (
+    <>
+      <Toaster position="top-center" />
+      <Radio />
+    </>
+  );
 }
 
 export default App;
