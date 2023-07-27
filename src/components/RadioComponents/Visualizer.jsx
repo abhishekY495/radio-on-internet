76aec166f345e0cea2d779fd59b69e48fb956b79
Abhishek <67749693+abhishekY495@users.noreply.github.com>
2023-07-27 23:52:20 +0530

    Styling

--- src/components/RadioComponents/Visualizer.jsx
+++ src/components/RadioComponents/Visualizer.jsx
@@ -8,27 +8,36 @@ export default function Visualizer() {
   const stationsData = useSelector((state) => state.radioStations);
   const { selectedStation } = stationsData;
 
-  return selectedStation ? (
-    <div className="h-[80%] px-2">
-      <AudioSpectrum
-        id="canvas"
-        height={318}
-        width={290}
-        audioId={"audio-element"}
-        capHeight={0}
-        meterWidth={3}
-        meterCount={80}
-        meterColor={[
-          { stop: 0, color: "gray" },
-          { stop: 0.5, color: "gray" },
-          { stop: 1, color: "gray" },
-        ]}
-        gap={4}
-      />
-    </div>
-  ) : (
-    <div className="h-[80%] flex opacity-20">
-      <img src={defaultVisualizer} className="pb-2" alt="default visualizer" />
-    </div>
+  return (
+    <>
+      <div className={`${selectedStation ? "block" : "hidden"} h-[80%] px-2`}>
+        <AudioSpectrum
+          id="canvas"
+          height={318}
+          width={290}
+          audioId={"audio-element"}
+          capHeight={0}
+          meterWidth={3}
+          meterCount={80}
+          meterColor={[
+            { stop: 0, color: "gray" },
+            { stop: 0.5, color: "gray" },
+            { stop: 1, color: "gray" },
+          ]}
+          gap={4}
+        />
+      </div>
+      <div
+        className={`${
+          selectedStation ? "hidden" : "block"
+        } h-[80%] flex opacity-20`}
+      >
+        <img
+          src={defaultVisualizer}
+          className="pb-2"
+          alt="default visualizer"
+        />
+      </div>
+    </>
   );
 }
