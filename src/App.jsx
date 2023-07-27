01c582fa1f26ecf004a671cb9e089df84b74e87c
Abhishek <67749693+abhishekY495@users.noreply.github.com>
2023-07-27 23:52:19 +0530

    Created Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { Toaster } from "react-hot-toast";
 
 import Radio from "./components/Radio";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <>
-      <Toaster position="bottom-center" />
+    <div className="flex flex-col items-center gap-10">
+      <Toaster position="top-center" />
       <Radio />
-    </>
+      <Footer />
+    </div>
   );
 }
 
---
+++ src/components/Footer.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+import githubLogo from "../assets/footer/github.png";
+
+export default function Footer() {
+  return (
+    <footer className="flex flex-col items-center gap-[2px] text-neutral-300/90">
+      <div>
+        Made by{" "}
+        <a
+          href="https://abhisheky495.netlify.app"
+          className="underline underline-offset-4 decoration-1 decoration-neutral-700 hover:opacity-80 transition-all"
+          target="_blank"
+        >
+          Abhishek
+        </a>
+      </div>
+      <div className="underline underline-offset-4 decoration-1 decoration-neutral-700 hover:opacity-80 transition-all">
+        <a
+          href="https://github.com/abhishekY495/radio-on-internet"
+          className="flex items-center gap-2"
+          target="_blank"
+        >
+          <img
+            src={githubLogo}
+            alt="github"
+            className="w-4 opacity-60 hover:opacity-80 transition-all"
+          />
+          Source code
+        </a>
+      </div>
+    </footer>
+  );
+}
