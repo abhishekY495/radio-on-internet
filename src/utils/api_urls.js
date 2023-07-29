7eca310ce932bf72001104dd3c78d97f3b9ffc04
Abhishek <67749693+abhishekY495@users.noreply.github.com>
2023-07-29 17:29:03 +0530

    Updated urls

--- src/utils/api_urls.js
+++ src/utils/api_urls.js
@@ -1,9 +1,9 @@
 import { createMetadataClient } from "@radiolise/metadata-client";
 
 export const COUNTRIES_API_URL =
-  "http://de1.api.radio-browser.info/json/countries";
+  "https://de1.api.radio-browser.info/json/countries";
 export const LANGUAGES_API_URL =
-  "http://de1.api.radio-browser.info/json/languages";
+  "https://de1.api.radio-browser.info/json/languages";
 export const RADIO_STATIONS_API_URL =
 "https://de1.api.radio-browser.info/json/stations/search?hidebroken=true&";
 export const RADIOLISE_WEB_SOCKET = createMetadataClient({
