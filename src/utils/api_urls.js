dae1865b6467df5049e727fc84e74120949a37bd
Abhishek <67749693+abhishekY495@users.noreply.github.com>
2023-08-05 13:58:00 +0530

    Added API urls

--- src/utils/api_urls.js
+++ src/utils/api_urls.js
@@ -1,11 +1,9 @@
 import { createMetadataClient } from "@radiolise/metadata-client";
 
-export const COUNTRIES_API_URL =
-  "https://de1.api.radio-browser.info/json/countries";
-export const LANGUAGES_API_URL =
-  "https://de1.api.radio-browser.info/json/languages";
-export const RADIO_STATIONS_API_URL =
-"https://de1.api.radio-browser.info/json/stations/search?hidebroken=true&";
+export const API_URL_1 = "https://de1.api.radio-browser.info";
+export const API_URL_2 = "https://fr1.api.radio-browser.info";
+export const API_URL_3 = "https://at1.api.radio-browser.info";
+
 export const RADIOLISE_WEB_SOCKET = createMetadataClient({
   url: "wss://backend.radiolise.com/api/data-service",
 });
